79b77f5ea80fecad54d83f81de1da4d0a28de8b9
aaronmhales <a02353892@aggies.usu.edu>
2023-10-04 15:15:31 -0600

    worked on contact page html and css a bit

---
+++ apps/web/frontend/src/routes/contact/+layout.svelte
@@ -0,0 +1,18 @@
+<style>
+    body {
+    	text-align: center;
+
+    }
+	form {
+        display: inline-block;
+    	width:fit-content;
+    	text-align: center;
+    }
+    div {
+    	padding: 3px;
+    }
+    label {
+    	display: block;
+    	text-align: left;
+	}
+</style>
---
+++ apps/web/frontend/src/routes/contact/+page.svelte
@@ -0,0 +1,49 @@
+<h1>Contact Manager</h1>
+<form method="POST">
+    <div>
+        <label for="name">
+            Name:
+        </label>
+        <input
+            type="text"
+            id="name"
+            name="name"
+            autocomplete="off"
+            placeholder="Your name"
+            required    
+        />
+    </div>
+
+    <div>
+        <label for="email">
+            Email:
+        </label>
+        <input
+            type="email"
+            id="email"
+            name="email"
+            autocomplete="off"
+            placeholder="Your email"
+            required
+        />
+    </div>
+
+    <div>
+        <label for="message">
+            message:
+        </label>
+        <textarea
+            id="message"
+            name="message"
+            autocomplete="off"
+            rows="10"
+            placeholder="Message to manager goes here."
+            required>
+        </textarea>
+    </div>
+
+    <div>
+        <input type="submit" value="Send Message"/>
+    </div>
+
+</form>
