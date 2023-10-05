85690d1c0efa4d5a4872d57b5a6ff255bcc1a530
aaronmhales <a02353892@aggies.usu.edu>
2023-10-05 13:32:12 -0600

    worked on track order pages and styling

---
+++ apps/web/frontend/src/routes/track_order/+page.svelte
@@ -0,0 +1,36 @@
+<h1>Tracking Order#: ???????</h1>
+<div id="order_tracking">
+    <p id="time_order">Order Date: MM/DD/YYYY HH:MM</p>
+    <p id="delivery_order">Delivered/Arriving: TIME</p>
+    <p id="cost_order">Order Total: $?????? (# cones)</p>
+    <div id="content_order">
+        <div id="cone">
+            <div class="details">
+                <ul>
+                    <li>Cone: ????</li>
+                    <li>Scoops:
+                        <ul>
+                            <li>option</li>
+                            <li>option</li>
+                            <li>option</li>
+                        </ul>
+                    </li>
+                    <li>Toppings:
+                        <ul>
+                            <li>options.</li>
+                            <li>options.</li>
+                            <li>options.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div id="buttons">
+                <p class="quantity">Qty. ??</p>
+                <div>
+                    <p><span class="add_to_cat_icon">h</span><p></p>
+                    <button class="add_to_cart" type="button">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
