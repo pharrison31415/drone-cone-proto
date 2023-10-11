ba4b47a55250101e51616092627d2a5b4abc77b6
aaronmhales <a02353892@aggies.usu.edu>
2023-10-11 15:20:53 -0600

    finished account page

--- apps/web/frontend/.svelte-kit/generated/client/app.js
+++ apps/web/frontend/.svelte-kit/generated/client/app.js
@@ -7,15 +7,29 @@ export const nodes = [
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6')
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9'),
+	() => import('./nodes/10'),
+	() => import('./nodes/11'),
+	() => import('./nodes/12'),
+	() => import('./nodes/13'),
+	() => import('./nodes/14'),
+	() => import('./nodes/15')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [4],
-		"/contact": [5,[2]],
-		"/track_order": [6,[3]]
+		"/": [8],
+		"/Manager_Page": [11,[3]],
+		"/Sign_In_Page": [12,[4]],
+		"/Sign_Up_Page_Customer": [13,[5]],
+		"/Sign_Up_Page_Drone": [14,[6]],
+		"/account": [9],
+		"/contact": [10,[2]],
+		"/track_order": [15,[7]]
 	};
 
 export const hooks = {
