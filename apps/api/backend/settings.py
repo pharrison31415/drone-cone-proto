e1db7a8b8b84c66fdadcdd9073a472d629aaaf27
TarynN <a02217840@aggies.usu.edu>
2023-10-13 11:08:59 -0700

    changed allowed hosts

--- apps/api/backend/settings.py
+++ apps/api/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h$#*5!w9c5m&z@(+slk21l#wf5ehlt*fa()$gvlyewi#np86m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
