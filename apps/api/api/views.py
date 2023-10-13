e720dd09032c9c036157761f5df5a330c530c779
Paul Harrison <a02382371@aggies.usu.edu>
2023-10-13 10:48:06 -0600

    endpoint /api/get-drone-statuses

--- apps/api/api/views.py
+++ apps/api/api/views.py
@@ -1,5 +1,20 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
+
+from api.models import DroneStatus
 
 
 def hello_world(request):
     return JsonResponse({"helloWorld": False})
+
+
+def get_drone_statuses(request):
+    statuses = DroneStatus.objects.all()
+
+    statuses_arr = []
+    for status in statuses:
+        statuses_arr.append({
+            "text": status.text
+        })
+
+    return JsonResponse({"droneStatuses": statuses_arr})
