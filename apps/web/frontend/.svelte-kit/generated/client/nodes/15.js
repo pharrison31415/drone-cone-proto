37497575f5e8cb733efa192a05eb866593197430
Paul Harrison <a02382371@aggies.usu.edu>
2023-10-13 17:09:44 -0600

    gitignore .svelte-kit directory

--- apps/web/frontend/.svelte-kit/generated/client/nodes/15.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/track_order/+page.svelte";
--- apps/web/frontend/.svelte-kit/generated/client/nodes/2.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/Manager_Page/+layout.svelte";
