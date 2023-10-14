2447fa49d519d6154afb1deea6940188717b9f42
Paul Harrison <a02382371@aggies.usu.edu>
2023-10-14 16:11:24 -0600

    new_customer view

--- apps/api/api/views.py
+++ apps/api/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.hashers import make_password, check_password
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from api.models import DroneStatus, Customer
 
@@ -19,7 +21,21 @@ def get_drone_statuses(request):
 
     return JsonResponse({"droneStatuses": statuses_arr})
 
+
+@csrf_exempt
 def new_customer(request):
-    username = request.POST['username']
-    #take the password sent over, hash it, save the hash to the database
+    if request.method != "POST":
+        return JsonResponse({'success': False, 'message': 'POST method required. Do not use these credentials.'})
+
+    # if username taken
+    if Customer.objects.filter(pk=request.POST['username']):
+        return JsonResponse({'success': False, 'message': 'username taken'})
+
+    Customer(
+        username=request.POST['username'],
+        passwordHash=make_password(request.POST['password']),
+        first_name=request.POST['firstName'],
+        last_name=request.POST['lastName'],
+    ).save()
+
     return JsonResponse({'success': True})
