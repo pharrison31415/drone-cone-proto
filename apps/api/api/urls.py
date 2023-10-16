5f25d040d8e72ed3145df45bae1fc7a21bc8b0c2
Alex Colqui <alexcolqui@gmail.com>
2023-10-16 14:45:21 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/pharrison31415/drone-cone-proto""
    This reverts commit 7e5fcefb15e0b0f61fb73ef482c89034edbd2bd7.

--- apps/api/api/urls.py
+++ apps/api/api/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('hello/', views.hello_world),
-    path('get-drone-statuses/', views.get_drone_statuses),
+    path('drone-statuses/', views.get_drone_statuses),
+    path('drone-types/', views.get_drone_types),
+    path('new-customer/', views.new_customer),
+    path('customer-login/', views.customer_login),
+    path('private-customer-data/', views.private_customer_data),
 ]
