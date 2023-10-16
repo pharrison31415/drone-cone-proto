4c9a7134a65847eb7c1275a184b9c05df4ff00b7
Alex Colqui <alexcolqui@gmail.com>
2023-10-16 14:44:25 -0600

    Revert "Updates to the frontend (front and manager pages)"
    This reverts commit 6ba79055bc2627fbdcc444804150ce85978958b7.

--- apps/web/frontend/src/routes/Manager_Page/+page.svelte
+++ apps/web/frontend/src/routes/Manager_Page/+page.svelte
@@ -1,20 +1,16 @@
 <h1>Manager Page</h1>
 
 <!--Inventory data-->
-
-<h2 class="centerText"> Inventory </h2>
 <div id="inventory" class="center" >
-
+  
 
 </div>
 
 <!--Revenue data-->
-<h2 class="centerText"> Revenue </h2>
 <div id="revenue" class="center">
 
 </div>
 <!--Contact data-->
-<h2 class="centerText"> Customer's Contact </h2>
 <div id="customer_contact"  class="center">
 
 </div>
@@ -28,74 +24,45 @@ TODO: api calls to GET data from data base for manager information
 
 
 <script>
-    import { onMount } from "svelte";
-// "localhost:8000/api/[data]" url for api call for data
-
-/*  
-**** Inventory Status ****
-onMount(async () => {
-  fetch("")
-  .then(response => response.json())
-  .then(data => {
-		inventory = data;  
-    }).catch(error => {
-    console.log(error);
-  });
-});
-
-**** Revenue Status ****
-onMount(async () => {
-  fetch('')
-  .then(response => response.json())
-  .then(revenueData => {
-		revenue = revenueData
-    }).catch(error => {
-    console.log(error);
-  });
-});
+	import { onMount } from "svelte";
 
 
-**** Contatct Status ****
-onMount(async () => {
-  fetch('')
-  .then(response => response.json())
-  .then(contactData => {
-		contact = contactData;  
-    }).catch(error => {
-    console.log(error);
-  });
-});
+    testSting = ""
+
+    /*  
+        contacts = None
+    	
+        async function  {
+		const response = await fetch('api request for list of contact');
+		contacts = await response.json();
+        }
+
+        revenue = None
+    	
+        async function  {
+		const response = await fetch('api request for revenue ');
+		revenue = await response.json();
+        }
 
 
-
-*/
-
+        inventory = None
+    	async function  {
+		const response = await fetch('api request for list of contact');
+		inventory = await response.json();
+        }   
+    */
 
 
 </script>
 
 <!--Temp Style **** RELOCATE to CSS FILE WHEN DONE ****-->
 <style>
-    h2 {
-        font-family: 'Trebuchet MS', sans-serif;
-    }
-
     .center {
         margin: auto;
         width: 50%;
         border: 3px solid rgb(0, 0, 0);
         padding: 10px;
-        font-family: 'Trebuchet MS', sans-serif;
-        }
-    
-
-    .centerText {
-        margin: auto;
-        width: 50%;
-        padding: 10px;
-        text-align: center;
-            }       
-
+            }
     #inventory {
         background:rgb(255, 255, 255);
         width:75%;
@@ -106,21 +73,19 @@ onMount(async () => {
             }
     #revenue {
         float:center; 
-        background:hsl(0, 0%, 100%);
+        background:hsl(189, 89%, 60%);
         width:75%;
         height:250px;
-        border-style: solid;
+        border-style: double;
         margin-bottom: 20px;
-        border-radius: 20px;
         }
     #customer_contact{
         float:center; 
-        background:rgb(249, 249, 249);
+        background:rgb(61, 247, 133);
         width:75%;
         height:250px;
-        border-style: solid;
+        border-style: double;
         margin-bottom: 20px;
-        border-radius: 20px;
             }
     
 </style>
