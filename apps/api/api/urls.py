6231cb7cbfdb9018d588a63546f659cc1ec883f5
Paul Harrison <a02382371@aggies.usu.edu>
2023-10-25 12:48:05 -0600

    format

--- apps/api/api/urls.py
+++ apps/api/api/urls.py
@@ -5,14 +5,18 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('hello/', views.hello_world),
+
     path('drone-statuses/', views.get_drone_statuses),
     path('drone-types/', views.get_drone_types),
+
     path('new-customer/', views.new_customer),
     # path('new-manager/', views.new_manager),
     path('new-owner/', views.new_owner),
+
     path('customer-login/', views.customer_login),
     path('manager-login/', views.manager_login),
     path('owner-login/', views.owner_login),
+    
     path('private-customer-data/', views.private_customer_data),
     path('private-manager-data/', views.private_manager_data),
     path('private-owner-data/', views.private_owner_data),
