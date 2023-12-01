4239cc522c6695f2d540052e228ddca0716adb66
TarynN <a02217840@aggies.usu.edu>
2023-12-01 11:51:04 -0700

    added path to urls and updated documentation

--- apps/api/api/urls.py
+++ apps/api/api/urls.py
@@ -44,6 +44,8 @@ urlpatterns = [
     path('manager-costs/', views.get_manager_costs),
     path('purchase-inventory/', views.purchase_inventory),
     path('update-inventory/', views.update_inventory),
+    path('messages/', views.get_messages),
 
     path('new-message/', views.new_message),
+    
 ]
